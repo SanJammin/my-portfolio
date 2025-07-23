2286ad0e42c3955ee3e5d755433ae2729e8d4748
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-07-23 19:32:45 +0100

    add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,9 @@ import Footer from "@/components/Footer";
 export const metadata = {
   title: "James Farr | Portfolio",
   description: "Frontend Developer Portfolio",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
